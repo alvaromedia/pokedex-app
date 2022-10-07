97b8d3dc1338c4b9c35763304bbfca4593dcd66e
alvaromedia <93778428+alvaromedia@users.noreply.github.com>
2022-10-07 18:25:43 +0200

    Added basic styling, functionality to fetch data from an external API and displaying it to the client

--- app.js
+++ app.js
@@ -1,40 +1,81 @@
 const pokemonRepository = (function () {
-  const pokemonArray = [
-    { name: 'Bulbasur', height: 0.7, type: ['grass', 'poison'] },
-    { name: 'Charmander', height: 0.6, type: ['fire'] },
-    { name: 'Squirtle', height: 0.5, type: ['water'] },
-  ];
+  const pokemonArray = [];
 
+  // Fetch the first 150 pokemon
+  function fetchPokemon() {
+    return fetch('https://pokeapi.co/api/v2/pokemon/?limit=151')
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        // For each pokemon create an object and push it into 'pokemonArray' with 'addPokemon' func
+        data.results.forEach((pokemonObj) => {
+          let pokemon = {
+            name: pokemonObj.name,
+            detailsUrl: pokemonObj.url,
+          };
+          addPokemon(pokemon);
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  // Fetch a single pokemon's details
+  function fetchPokemonDetails() {}
+
+  // Get the whole pokemon array
   function getAllPokemon() {
     return pokemonArray;
   }
 
-  function addPokemon(pokemon) {
-    pokemonArray.push(pokemon);
+  // Add a new single pokemon object to the array
+  function addPokemon(pokemonObj) {
+    if (typeof pokemonObj !== 'object') return;
+    pokemonArray.push(pokemonObj);
   }
 
+  // Filter pokemon by name
+  function filterPokemon(pokemonName) {
+    return pokemonArray.filter((pokemonObj) => pokemonObj.name === pokemonName);
+  }
+
+  // With a given pokemon, create a button/list-item and insert it into the DOM
+  function addListItem(pokemonObj) {
+    const pokemonList = document.querySelector('.pokemon-list');
+    const pokemonListItem = document.createElement('li');
+    const pokemonButton = document.createElement('button');
+
+    pokemonButton.innerText = pokemonObj.name;
+    pokemonButton.classList.add('pokemon-button');
+
+    pokemonList.appendChild(pokemonListItem);
+    pokemonListItem.appendChild(pokemonButton);
+
+    // Show details
+    showDetails(pokemonButton, pokemonObj.name);
+  }
+
+  // On a click event in a button, log the pokemon name to the console
+  function showDetails(button, pokemonName) {
+    button.addEventListener('click', () => {
+      console.log(pokemonName);
+    });
+  }
+
+  // Returned value from the IIFE
   return {
+    fetchPokemon,
     getAllPokemon,
     addPokemon,
+    filterPokemon,
+    addListItem,
   };
 })();
 
-// Add a new pokemon
-pokemonRepository.addPokemon({
-  name: 'Pikachu',
-  height: 0.4,
-  type: ['electric'],
-});
-
-// Log the pokemon array to the console
-console.log(pokemonRepository.getAllPokemon());
-
-// Create a function to log the pokemon's name
-function printPokemonDetails(array) {
-  array.forEach((pokemon) => {
-    console.log(pokemon.name);
+pokemonRepository.fetchPokemon().then(() => {
+  pokemonRepository.getAllPokemon().forEach((pokemonObj) => {
+    pokemonRepository.addListItem(pokemonObj);
   });
-}
-
-// Invoke the function with the actual argument
-printPokemonDetails(pokemonRepository.getAllPokemon());
+});
